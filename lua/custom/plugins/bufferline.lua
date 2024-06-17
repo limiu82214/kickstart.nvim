8792a9257b3fd0e2830dda8266876a6484463549
limiu82214 <limiu82214@gmail.com>
2024-06-17 22:07:26 +0800

    bufferline

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,42 @@
+return {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+        require'bufferline'.setup {
+            options = {
+                numbers = "none",
+                number_style = "superscript",
+                mappings = true,
+                close_command = "bdelete! %d",
+                right_mouse_command = "bdelete! %d",
+                left_mouse_command = "buffer %d",
+                middle_mouse_command = nil,
+                indicator_icon = '▎',
+                buffer_close_icon = '',
+                modified_icon = '●',
+                close_icon = '',
+                left_trunc_marker = '',
+                right_trunc_marker = '',
+                name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
+                    return buf.name
+                end,
+                max_name_length = 18,
+                max_prefix_length = 15, -- prefix used when a buffer is deduplicated
+                tab_size = 18,
+                diagnostics = "nvim_lsp",
+                diagnostics_update_in_insert = false,
+                offsets = {{filetype = "NvimTree", text = "File Explorer", text_align = "left"}},
+                show_buffer_icons = true, -- disable filetype icons for buffers
+                show_buffer_close_icons = true,
+                show_close_icon = true,
+                show_tab_indicators = true,
+                persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+                separator_style = "thin",
+                enforce_regular_tabs = false,
+                always_show_bufferline = true,
+                sort_by = 'id'
+            },
+        }
+    end
+}
